4544da7ba5dd952706113e92fe638fd9d7cc0c30
KopiBanszky <kopibanszky@gmail.com>
2024-05-12 12:44:59 +0200

    sort

--- lib/pages/HomaPage.dart
+++ lib/pages/HomaPage.dart
@@ -85,8 +85,8 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
         SettingsDialog(
           itemType: ItemType.LARGE,
           categoryType: ItemType.LARGE,
-          order: "ASC",
-          orderBy: "create",
+          order: Order.ASC,
+          orderBy: SortBy.ID,
           columns: 1,
           oldSchool: false,
         );
@@ -101,8 +101,14 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
               }
             : null,
         moreFunction: () {
+          Order currentOrder = settings!.order;
+          SortBy currentOrderBy = settings!.orderBy;
           settings!.display(context).then((value) {
             if (value) {
+              if(currentOrderBy != settings!.getOrderBy() || currentOrder != settings!.getOrder()){
+                items.sortItemsBy(settings!.getOrderBy(), settings!.getOrder());
+                categories.sortItemsBy(settings!.getOrderBy(), settings!.getOrder());
+              }
               loadItems();
               loadCategories();
             }
@@ -113,12 +119,17 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
     if (categories.items.isEmpty) {
       await categories.getCategories(
         arguments?["route"] ?? "default",
+        order: ToStr.order(settings!.order),
+        orderBy: ToStr.sortBy(settings!.orderBy),
       );
     }
     loadCategories();
 
     if (items.items.isEmpty) {
-      await items.getItems(arguments?["route"] ?? "default", updateOnLoad: true,
+      await items.getItems(arguments?["route"] ?? "default", 
+      order: ToStr.order(settings!.order),
+      orderBy: ToStr.sortBy(settings!.orderBy),
+      updateOnLoad: true,
           onLoad: () {
         loadItems();
         setState(() {});
